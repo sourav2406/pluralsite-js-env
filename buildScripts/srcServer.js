985e41d53ac516c1b3a77195825e79e12520911f
Sourav Mandal <sourav2406@gmail.com>
2017-08-03 16:22:11 +0530

    http mock api etc

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users', function(req,res){
+  //hard coded api return call
+  res.json([
+    {"id":1,"firstname":"Sourav","lastname":"Mandal","email":"sourav123@abc.kj"},
+    {"id":2,"firstname":"Sam","lastname":"Son","email":"sam123@abc.kj"},
+    {"id":3,"firstname":"Matheu","lastname":"lee","email":"matheu123@abc.kj"}
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
